aa80bb39b9211f8f7f11749d94105d3a26d32a12
Steven Han <wufufufu@gmail.com>
2022-10-11 12:03:29 -0500

    Support ActiveRecord 6.1+

--- lib/pgpool_no_load_balance/active_record/connection_adapters/postgresql_adapter.rb
+++ lib/pgpool_no_load_balance/active_record/connection_adapters/postgresql_adapter.rb
@@ -9,12 +9,12 @@ module PgpoolNoLoadBalance
 
         private
 
-        def to_sql_and_binds(arel_or_sql_string, binds=[])
-          sql, binds = super
+        def to_sql_and_binds(arel_or_sql_string, binds = [], preparable = nil) # :nodoc:
+          sql, binds, preparable = super
           if arel_or_sql_string.respond_to?(:pgpool_nlb?) && arel_or_sql_string.pgpool_nlb?
             sql = "#{NLB_COMMENT} #{sql}"
           end
-          [sql.freeze, binds]
+          [sql.freeze, binds, preparable]
         end
       end
     end
